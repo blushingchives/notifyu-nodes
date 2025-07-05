bae74fedc5dad9e10dfd3e9145fe5ff806c730f6
blushingchives <blushing@chives.com>
2025-07-05 22:38:18 +0800

    fix

--- snapshot/entrypoint.sh
+++ snapshot/entrypoint.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 
-# Get current snapshot file in /data
-CURRENT_HEIGHT=$(ls /data/*.tar.gz 2>/dev/null | grep -Eo "[0-9]+.tar.gz" | cut -d "." -f1)
+# Get current snapshot height from /data (empty if none)
+CURRENT_HEIGHT=$(ls /data/*.tar.gz 2>/dev/null | grep -Eo "[0-9]+.tar.gz" | cut -d "." -f1 || echo "")
 
 # Get latest snapshot filename from the internet
 LATEST_SNAPSHOT=$(curl -s "https://snapshots.ninerealms.com/snapshots?prefix=thornode" | grep -Eo "thornode/[0-9]+.tar.gz" | sort -n | tail -n 1 | cut -d "/" -f2)
 LATEST_HEIGHT=$(echo $LATEST_SNAPSHOT | cut -d "." -f1)
 
-echo "Current Height: $CURRENT_HEIGHT, Latest Height: $LATEST_HEIGHT"
-
-# Compare and skip if same
-if [[ "$CURRENT_HEIGHT" == "$LATEST_HEIGHT" ]]; then
-    echo "Skipping snapshot."
-    exit 0
+# If no local snapshot or heights differ, download and extract
+if [[ "$CURRENT_HEIGHT" != "$LATEST_HEIGHT" ]]; then
+    rm -rf /data/*
+    aria2c --split=16 --max-concurrent-downloads=16 --max-connection-per-server=16 --continue --min-split-size=100M -d /data -o $LATEST_SNAPSHOT "https://snapshots.ninerealms.com/snapshots/thornode/${LATEST_SNAPSHOT}"
+    echo "Extracting $LATEST_SNAPSHOT."
+    pv --force /data/$LATEST_SNAPSHOT | tar -xzf - -C /data
+    echo "Extracting $LATEST_SNAPSHOT."
 fi
 
-rm -rf /data/*
-FILENAME=$(curl -s "https://snapshots.ninerealms.com/snapshots?prefix=thornode" | grep -Eo "thornode/[0-9]+.tar.gz" | sort -n | tail -n 1 | cut -d "/" -f 2)
-aria2c --split=16 --max-concurrent-downloads=16 --max-connection-per-server=16 --continue --min-split-size=100M -d /data -o $FILENAME "https://snapshots.ninerealms.com/snapshots/thornode/${FILENAME}"
-echo "Extracting $FILENAME."
-pv --force /data/$FILENAME | tar -xzf - -C /data
-echo "Extracting $FILENAME."
-echo "Removing tar file."
-rm -rf /data/$FILENAME
-echo "Done. Snapshot process complete.
+echo "Done. Snapshot process complete."
